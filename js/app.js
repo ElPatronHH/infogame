df24d475ecbc8b19a7e6f068c6e6f93dfcf4f5d4
ElPatronHH <aa.roockie@gmail.com>
2023-10-24 11:22:33 -0600

    Bosquejo de la página

---
+++ js/app.js
@@ -0,0 +1,44 @@
+const listaJuegos = document.querySelector("#lista-juegos");
+
+document.addEventListener("DOMContentLoaded", function () {
+  let juegosArray = [];
+  const leerJSONJuegos = () => {
+    const url = "juegos.json";
+    fetch(url)
+      .then((respuesta) => respuesta.json())
+      .then((data) => {
+        juegosArray = data;
+        mostrarJuegos();
+      })
+      .catch((error) => {
+        console.error("Error al cargar el archivo JSON:", error);
+      });
+  };
+  leerJSONJuegos();
+  function mostrarJuegos() {
+    listaJuegos.innerHTML = "";
+    let contador = 0;
+    juegosArray.forEach((juego, index) => {
+      const juegoDiv = document.createElement("div");
+      juegoDiv.className = "three columns";
+      juegoDiv.innerHTML = `
+                <div class="card">
+                    <img src="${juego.imagen}" class="u-full-width">
+                    <div class="info-card">
+                        <h4>${juego.titulo}</h4>
+                        <p>${juego.descripcion}</p>
+                        <a href="${juego.ruta}" class="button-primary button" data-id="${juego.id}">Aprender Más</a>
+                    </div>
+                </div>
+            </div>
+            `;
+      if (contador % 4 === 0) {
+        const rowDiv = document.createElement("div");
+        rowDiv.className = "row";
+        listaJuegos.appendChild(rowDiv);
+      }
+      listaJuegos.lastChild.appendChild(juegoDiv);
+      contador++;
+    });
+  }
+});
